0a0fa2983fae4a4f8cfa1dd446356a7eb08d792e
Andrew <95236304+loneew@users.noreply.github.com>
2023-05-10 20:05:42 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from connection.connection_pool import MySQLConnectionPool
-from dao_impl.author_dao_impl import AuthorDaoImpl
-from dao_impl.book_dao_impl import BookDaoImpl
-from dao_impl.user_dao_impl import UserDaoImpl
+from DAO.dao_impl.author_dao_impl import AuthorDaoImpl
+from DAO.dao_impl.book_dao_impl import BookDaoImpl
+from DAO.dao_impl.user_dao_impl import UserDaoImpl
 from models.user import User
 
 
@@ -11,12 +11,11 @@ def main():
     user_table_2 = BookDaoImpl(pool)
     user_table_3 = AuthorDaoImpl(pool)
     user_1 = User(13, "Jack", "Black", "jack_bl@gmail.com", "+380987645120", "12345")
-    user_table_1.delete(12)
-    user_table_1.add(user_1)
-    user_table_1.read_by_id(13)
-    user_table_2.read_by_id(1)
-    user_table_3.read_by_id(1)
-
+    #user_table_1.delete(12)
+    #user_table_1.add(user_1)
+    #user_table_1.read_by_id(13)
+    #user_table_2.read_by_id(1)
+    #user_table_3.read_by_id(1)
 
 
 if __name__ == "__main__":
